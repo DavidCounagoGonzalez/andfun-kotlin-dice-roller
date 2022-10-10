f9718d9db9cc5acb7807280de80161244cd0c854
David <dcounagogonzalez@danielcastelao.org>
2022-10-10 09:53:48 +0200

    Añadidas imágenes sin sentido

--- app/src/main/java/com/example/android/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/android/diceroller/MainActivity.kt
@@ -19,8 +19,6 @@ package com.example.android.diceroller
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
-import android.widget.TextView
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import java.util.*
 
@@ -42,12 +40,12 @@ class MainActivity : AppCompatActivity() {
     private fun rollDice() {
         val randomInt = Random().nextInt(6) + 1
         val drawableResource = when (randomInt) {
-            1 -> R.drawable.dice_1
-            2 -> R.drawable.dice_2
-            3 -> R.drawable.dice_3
-            4 -> R.drawable.dice_4
-            5 -> R.drawable.dice_5
-            else -> R.drawable.dice_6
+            1 -> R.drawable.frog
+            2 -> R.drawable.poppy
+            3 -> R.drawable.lechonk
+            4 -> R.drawable.what
+            5 -> R.drawable.croissant
+            else -> R.drawable.clover
         }
 
         diceImage.setImageResource(drawableResource)
